1806a93afdecaa3edf9c83730727f6bc4938f148
alekunkel <106685415+alekunkel@users.noreply.github.com>
2023-09-03 15:35:37 -0300

    ahora el js anda, falta hacer que cambien las imagenes y que lapágina quede facherita

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,29 +1,69 @@
-let energia = 100;
-let sueno = 0;
+class Pou {
+  constructor() {
+    this.energia = 100;
+    this.energiaMax = 100;
+    this.sueno = 0;
+    this.suenoMax = 100;
+  }
 
-function actualizarValores() {
-  document.getElementById("energia").textContent = energia + "%";
-  document.getElementById("sueño").textContent = sueno + "%";
+  getEnergia() {
+    return this.energia;
+  }
+
+  getSueno() {
+    return this.sueno;
+  }
+
+  correr() {
+    const energiaConsumida = this.energiaMax * 0.25;
+    if (this.energia >= energiaConsumida) {
+      this.energia -= energiaConsumida;
+      this.sueno += 25;
+      return "Estoy corriendo";
+    } else {
+      return "Estoy cansado jefe, no puedo correr";
+    }
+  }
+
+  dormir() {
+    if (this.sueno === 0) {
+      return "¡Ya dormí mucho!";
+    }
+    const energiaRecuperada = this.energiaMax * 0.25;
+    this.energia += energiaRecuperada;
+    this.sueno -= 25;
+    if (this.sueno < 0) {
+      this.sueno = 0;
+    }
+    if (this.energia > this.energiaMax) {
+      this.energia = this.energiaMax;
+    }
+    return "Estoy durmiendo";
+  }
+}
+
+var pou = new Pou();
+
+document.addEventListener("DOMContentLoaded", function () {
+  refrescar();
+});
+
+function refrescar() {
+  document.getElementById("energia").textContent = pou.getEnergia();
+  document.getElementById("sueno").textContent = pou.getSueno();
+  if (pou.getEnergia() > 80) {
+    document.getElementById("neymarTristeContainer").style.display = "none";
+  } else {
+    document.getElementById("neymarTristeContainer").style.display = "block";
+  }
 }
 
 function correr() {
-  if (energia >= 25) {
-    energia = Math.max(0, energia - 25); // Evitar valores negativos
-    sueno = Math.min(100, sueno + 25); // Evitar que el sueño supere 100%
-    actualizarValores();
-  } else {
-    alert("¡No tienes suficiente energía para correr!");
-  }
+  alert(pou.correr());
+  refrescar();
 }
 
 function dormir() {
-  if (sueno >= 25) {
-    sueno = Math.max(0, sueno - 25); // Evitar valores negativos
-    energia = Math.min(100, energia + 25); // Evitar que la energía supere 100%
-    actualizarValores();
-  } else {
-    alert("¡No estás lo suficientemente cansado para dormir!");
-  }
+  alert(pou.dormir());
+  refrescar();
 }
-
-actualizarValores();
